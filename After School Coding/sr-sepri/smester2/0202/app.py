35cd7f2bf6a1b6cc1465c6044a01076be35f09a6
George A <Georgearrev@gmail.com>
2026-02-02 17:30:07 +0700

    Renew app.py (2/2/2026)

--- After School Coding/sr-sepri/smester2/0202/app.py
+++ After School Coding/sr-sepri/smester2/0202/app.py
@@ -2,11 +2,24 @@ from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
+usernameKey = "admin"
+passwordKey = "admin123"
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        return "validation"
+        username = request.form['username']
+        password = request.form['password']
+        if username == usernameKey and password == passwordKey:
+            session['user'] = True
+            return render_template('admin.html')
     return render_template('home.html')
+
+@app.route('/admin')
+def admin():
+    if 'user' in session:
+        return render_template('admin.html')
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
