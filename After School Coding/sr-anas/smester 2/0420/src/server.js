d094ebba01d6c2d78a73aa447a6b23afcfaba514
George A <Georgearrev@gmail.com>
2026-04-20 17:46:39 +0700

    4/20/2026 - 17:46

--- After School Coding/sr-anas/smester 2/0420/src/server.js
+++ After School Coding/sr-anas/smester 2/0420/src/server.js
@@ -3,9 +3,28 @@ const app = express();
 const port = 5000;
 const host = "127.0.0.1";
 
+app.get("/", (_, res) => {
+    res.send("Hello, Express!");
+});
+
+app.post("/", (_, res) => {
+    res.send("Hello, Express! With Post Method");
+});
+
+app.patch("/", (_, res) => {
+    res.send("Hello, Express! With Patch Method");
+});
+
+app.put("/", (_, res) => {
+    res.send("Hello, Express! With Put Method");
+});
+
+app.delete("/", (_, res) => {
+    res.send("Hello, Express! With Delete Method");
+});
 
 app.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
 
-// Run command: node ./src/server.js
+// Run command: npm run dev
